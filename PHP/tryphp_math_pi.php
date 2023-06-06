739f93ee6bf2fbc03d423d2db253355586047974
AgusSunaryo <120612579+agussunaryo@users.noreply.github.com>
2023-06-06 19:14:01 +0700

    Create tryphp_math_pi.php

---
+++ PHP/tryphp_math_pi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+echo(pi(). " = pi <br>");
+
+echo(min(0, 150, 30, 20, -8, -200) . " = Min <br>");
+echo(max(0, 150, 30, 20, -8, -200) . " = Max <br>");
+
+// case-sensitive constant name
+define("GREETING", "Welcome to W3Schools.com!");
+echo GREETING;
+
+?>
+
+</body>
+</html>

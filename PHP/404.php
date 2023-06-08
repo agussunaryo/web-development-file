0b1e49b0e17b9d11b858b67d05ed723ce5003898
AgusSunaryo <120612579+agussunaryo@users.noreply.github.com>
2023-06-08 22:48:28 +0700

    Update 404.php

--- PHP/404.php
+++ PHP/404.php
@@ -1,16 +1,3 @@
 <?php
-/**
- * The template for displaying 404 pages.
- *
- * @package OceanWP WordPress theme
- */
 
-// Get ID.
-$get_id = get_theme_mod( 'ocean_error_page_template' );
-
-// Check if page is Elementor page.
-$elementor = get_post_meta( $get_id, '_elementor_edit_mode', true );
-
-// Get content.
-$get_content = oceanwp_error_page_template_content();
 
